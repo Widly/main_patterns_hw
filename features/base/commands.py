b4bca4a0b9bb9b2b2d3714d51669b76a35f060eb
i.rumyantsev <igorrum@mail.ru>
2023-01-17 13:55:29 +0300

    add adapter's generation

--- features/base/commands.py
+++ features/base/commands.py
@@ -1,6 +1,8 @@
+import inspect
 from typing import Iterable
 
 from exceptions import CommandException
+from iocs import IoC
 from .interfaces import ICommand
 
 
@@ -14,3 +16,94 @@ class MacroCommand(ICommand):
                 cmd.execute()
         except Exception as e:
             raise CommandException(e)
+
+
+class CreateAdapterException(Exception):
+    pass
+
+
+class InitCreateCommandAdapterStrategy(ICommand):
+    """
+    Данная команда регистрирует в IoC стратегию которая:
+        1. Генерирует адаптер для произвольного интерфейса
+        2. Оборачивает переданный игровой объект в этот адаптер
+
+    Адаптер позволяет позволяет взаимодействовать с игровыми объектами не напрямую, а через IoC, благодаря чему
+    возможно переопределять логику взаимодействия с объектами на лету.
+
+    Например, для следующего интерфейса:
+
+        class IActionable:
+            def get_field_1(self):
+                ...
+
+            def set_field_1(self, value):
+                ...
+
+            def get_field_2(self):
+                ...
+
+    В результате вызова:
+        IoC.resolve("Adapter", IActionable, obj)
+
+    Будет сгенерирован следующий адаптер:
+
+        class IActionableAdapter(IActionable):
+            def __init__(self, obj: UObject):
+                self.obj = obj
+
+            def get_field_1(self):
+                return IoC.resolve('IActionable:field_1.get', self.obj)
+
+            def set_field_1(self, value):
+                IoC.resolve('IActionable:field_1.set', self.obj, new_v).execute()
+
+            def get_field_2(self):
+                return IoC.resolve('IActionable:field_2.get', self.obj)
+    """
+
+    adapters = {}  # Ранее созданные адаптеры
+
+    def function_factory(self, interface, f_name):
+        """Создает метод для генерируемого класса адаптера"""
+        op_type, property_name = f_name.split('_', 1)
+        ioc_path = f'{interface.__name__}:{property_name}.{op_type}'
+
+        if op_type == 'set':
+            def new_f(self, *args, **kwargs):
+                IoC.resolve(ioc_path, self.obj, *args, **kwargs).execute()
+        elif op_type == 'get':
+            def new_f(self, *args, **kwargs):
+                return IoC.resolve(ioc_path, self.obj, *args, **kwargs)
+        else:
+            raise CreateAdapterException(f"'{op_type}' is not allowd operation type!")
+
+        return new_f
+
+    def create_adapter(self, interface):
+        """Создание адаптера"""
+        if interface in self.adapters:
+            # Пользуемся адаптерами созданными ранее
+            return self.adapters[interface]
+
+        def constructor(self, obj):
+            self.obj = obj
+
+        class_methods = {
+            '__init__': constructor
+        }
+
+        for f_name, f in inspect.getmembers(interface, predicate=inspect.isfunction):
+            class_methods[f_name] = self.function_factory(interface, f_name)
+
+        adapter = type(f'{interface.__name__}Adapter', (interface,), class_methods)
+        self.adapters[interface] = adapter
+
+        return adapter
+
+    def execute(self) -> None:
+        IoC.resolve(
+            'IoC.Register',
+            'Adapter',
+            lambda interface, obj: self.create_adapter(interface)(obj)
+        ).execute()
--- features/base/interfaces.py
+++ features/base/interfaces.py
@@ -5,3 +5,13 @@ class ICommand(ABC):
     @abstractmethod
     def execute(self) -> None:
         ...
+
+
+class UObject(ABC):
+    @abstractmethod
+    def get_property(self, key: str) -> object:
+        ...
+
+    @abstractmethod
+    def set_property(self, key: str, value: object) -> None:
+        ...
